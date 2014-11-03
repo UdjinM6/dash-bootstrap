f5f3116c75f28e350862ceae9d8f4084fcc27a71
UdjinM6 <UdjinM6@users.noreply.github.com>
2014-11-03 20:17:27 +0300

    donations address

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,9 @@ cat ~/.darkcoin/blocks/blk0000* >> $file
 date=`date --utc`
 url=`curl --upload-file $file https://transfer.sh/bootstrap.dat`
 readmeHead="bootstrap.dat files for DarkCoin\n==="
-prevLinks=`tail -n +3 README.md | head`
-echo -e "$readmeHead\n$date [$url]($url)\n\n$prevLinks" > README.md
+donations="Donations are welcome:\n\nDRK: XsV4GHVKGTjQFvwB7c6mYsGV3Mxf7iser6"
+prevLinks=`tail -n +3 README.md | head -n -3 | head`
+echo -e "$readmeHead\n$date [$url]($url)\n\n$prevLinks\n\n$donations" > README.md
 rm $file
 git commit -am "$date - autoupdate"
 git push
