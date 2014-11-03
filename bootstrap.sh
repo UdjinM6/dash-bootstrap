49d4a17cb2142a5280dc6ce996b837fd236aed59
UdjinM6 <UdjinM6@users.noreply.github.com>
2014-11-03 19:17:35 +0300

    clean sh file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 file="bootstrap.dat"
-#file=~/test.txt
 cat ~/.darkcoin/blocks/blk0000* >> $file
 date=`date --utc`
 url=`curl --upload-file $file https://transfer.sh/bootstrap.dat`
 readmeHead="bootstrap.dat files for DarkCoin\n==="
 prevLinks=`tail -n +3 README.md | head`
 echo -e "$readmeHead\n$date [$url]($url)\n\n$prevLinks" > README.md
-#cat README.md
 rm $file
 git commit -am "$date - autoupdate"
 git push
