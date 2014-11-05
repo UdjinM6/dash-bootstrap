2c39762de57ccade01187d9ad6d5769e0a7636ba
UdjinM6 <UdjinM6@users.noreply.github.com>
2014-11-05 05:05:16 +0300

    fix: enable production functionality for shell script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 file="bootstrap.dat"
-#cat ~/.darkcoin/blocks/blk0000* >> $file
+cat ~/.darkcoin/blocks/blk0000* >> $file
 date=`date --utc`
-#url=`curl --upload-file $file https://transfer.sh/bootstrap.dat`
+url=`curl --upload-file $file https://transfer.sh/bootstrap.dat`
 header=`cat header.md`
 prevLinks=`head links.md`
 footer=`cat footer.md`
 newLinks="$date [$url]($url)\n\n$prevLinks"
 echo -e "$newLinks" > links.md
 echo -e "$header\n\n$newLinks\n\n$footer" > README.md
-#rm $file
-#git commit -am "$date - autoupdate"
-#git push
+rm $file
+git commit -am "$date - autoupdate"
+git push
