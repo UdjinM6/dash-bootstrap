c3b9d8617f3ed753dce018ad7a3cdd661290f1f5
UdjinM6 <UdjinM6@users.noreply.github.com>
2014-11-05 23:50:30 +0300

    md5 and sha256 signatures

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 file="bootstrap.dat"
+file_md5="md5.txt"
+file_sha256="sha256.txt"
 cat ~/.darkcoin/blocks/blk0000* >> $file
+touch $file_md5 $file_sha256
+md5sum $file > $file_md5
+sha256sum $file > $file_sha256
 date=`date --utc`
 url=`curl --upload-file $file https://transfer.sh/bootstrap.dat`
+url_md5=`curl --upload-file $file_md5 https://transfer.sh/$file_md5`
+url_sha256=`curl --upload-file $file_sha256 https://transfer.sh/$file_sha256`
 header=`cat header.md`
 prevLinks=`head links.md`
 footer=`cat footer.md`
-newLinks="$date [$url]($url)\n\n$prevLinks"
+newLinks="$date [$url]($url) [MD5]($url_md5) [SHA256]($url_sha256)\n\n$prevLinks"
 echo -e "$newLinks" > links.md
 echo -e "$header\n\n$newLinks\n\n$footer" > README.md
-rm $file
+rm $file $file_md5 $file_sha256
 git commit -am "$date - autoupdate"
 git push
