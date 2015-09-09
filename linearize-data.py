39091b1291059efa90399f4639f9b410cc48e573
UdjinM6 <UdjinM6@users.noreply.github.com>
2015-09-09 05:04:29 +0300

    darkcoin_hash -> dash_hash

--- linearize-data.py
+++ linearize-data.py
@@ -16,7 +16,7 @@ import base64
 import httplib
 import sys
 import hashlib
-import darkcoin_hash
+import dash_hash
 import datetime
 import time
 from collections import namedtuple
@@ -54,7 +54,7 @@ def calc_hdr_hash(blk_hdr):
 	#hash2_o = hash2.digest()
 
 	#return hash2_o
-        pow_hash = darkcoin_hash.getPoWHash(blk_hdr)
+        pow_hash = dash_hash.getPoWHash(blk_hdr)
         return pow_hash
 
 def calc_hash_str(blk_hdr):
