4d1ca6c7eece422e107ed33ed30668927eb33bca
UdjinM6 <UdjinM6@users.noreply.github.com>
2017-02-11 18:03:03 +0300

    clean up earlier

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,34 +15,50 @@ footer=`cat footer.md`
 #cat ~/.dash/blocks/blk0000* > $file
 ./linearize-hashes.py linearize.cfg > hashlist.txt
 ./linearize-data.py linearize.cfg 
+#
 touch $file_sha256
+sha256sum $file > $file_sha256
+#7z
 7z a $file_7z $file
-zip $file_zip $file
-sha256sum $file $file_7z $file_zip > $file_sha256
+sha256sum $file_7z >> $file_sha256
 size_7z=`ls -lh $file_7z | awk -F" " '{ print $5 }'`
-size_zip=`ls -lh $file_zip | awk -F" " '{ print $5 }'`
 url_7z=`curl --upload-file $file_7z https://transfer.sh/$file_7z`
+rm $file_7z
+#zip
+zip $file_zip $file
+sha256sum $file_zip >> $file_sha256
+size_zip=`ls -lh $file_zip | awk -F" " '{ print $5 }'`
 url_zip=`curl --upload-file $file_zip https://transfer.sh/$file_zip`
+rm $file_zip
+#
 url_sha256=`curl --upload-file $file_sha256 https://transfer.sh/$file_sha256`
 newLinks="Block $blocks: $date [7z]($url_7z) ($size_7z) [zip]($url_zip) ($size_zip) [SHA256]($url_sha256)\n\n$prevLinks"
 echo -e "$newLinks" > links.md
-rm $file $file_7z $file_zip $file_sha256 hashlist.txt
+rm $file $file_sha256 hashlist.txt
 #testnet
 #cat ~/.dash/testnet3/blocks/blk0000* > $file
 ./linearize-hashes.py linearize-testnet.cfg > hashlist.txt
 ./linearize-data.py linearize-testnet.cfg 
+#
 touch $file_sha256
+sha256sum $file > $file_sha256
+#7z
 7z a $file_7z $file
-zip $file_zip $file
-sha256sum $file $file_7z $file_zip > $file_sha256
+sha256sum $file_7z >> $file_sha256
 size_7z=`ls -lh $file_7z | awk -F" " '{ print $5 }'`
-size_zip=`ls -lh $file_zip | awk -F" " '{ print $5 }'`
 url_7z=`curl --upload-file $file_7z https://transfer.sh/$file_7z`
+rm $file_7z
+#zip
+zip $file_zip $file
+sha256sum $file_zip >> $file_sha256
+size_zip=`ls -lh $file_zip | awk -F" " '{ print $5 }'`
 url_zip=`curl --upload-file $file_zip https://transfer.sh/$file_zip`
+rm $file_zip
+#
 url_sha256=`curl --upload-file $file_sha256 https://transfer.sh/$file_sha256`
 newLinksTestnet="Block $blocksTestnet: $date [7z]($url_7z) ($size_7z) [zip]($url_zip) ($size_zip) [SHA256]($url_sha256)\n\n$prevLinksTestnet"
 echo -e "$newLinksTestnet" > linksTestnet.md
-rm $file $file_7z $file_zip $file_sha256 hashlist.txt
+rm $file $file_sha256 hashlist.txt
 #construct README.md
 echo -e "$header\n\n####For mainnet:\n\n$newLinks\n\n####For testnet:\n\n$newLinksTestnet\n\n$footer" > README.md
 #push
